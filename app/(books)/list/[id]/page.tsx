943ada85d51d75de8129b747e1bcd59ebcc13848
suyeonMin <ja163318@naver.com>
2025-04-14 00:10:29 +0900

    타입에러 수정.....

--- app/(books)/list/[id]/page.tsx
+++ app/(books)/list/[id]/page.tsx
@@ -2,32 +2,38 @@ import Link from "next/link";
 import style from "../../../styles/book.module.css";
 
 const URL = "https://books-api.nomadcoders.workers.dev/list";
-async function getBooks(id:string){
-	const res = await fetch(`${URL}?name=${id}`);
-	const json = await res.json();
-	return json;
+
+async function getBooks(id: string) {
+  const res = await fetch(`${URL}?name=${id}`);
+  const json = await res.json();
+  return json;
 }
 
-export default async function Books({
-	params,
-}: {
-	params: {id: string}
-}){
-	const books = await getBooks(params.id);
-	const list = books.results.books;
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-  return <div className={style.bookPage}>
-		<h1>{books.results.list_name}</h1>
+export default async function Page({ params }: PageProps) {
+  const books = await getBooks(params.id);
+  const list = books.results.books;
 
-		<ul>
-			{list.map(item => (
-				<li key={item?.title}>
-					<img src={item?.book_image} alt="book image" />
-					<p className={style.title}>{item?.title}</p>
-					<p className={style.author}>{item?.author}</p>
-					<Link href={`${item.amazon_product_url}`} target="_blank">Buy Now →</Link>
-				</li>
-				))}
-		</ul>
-	</div>
+  return (
+    <div className={style.bookPage}>
+      <h1>{books.results.list_name}</h1>
+      <ul>
+        {list.map((item: any) => (
+          <li key={item?.title}>
+            <img src={item?.book_image} alt="book image" />
+            <p className={style.title}>{item?.title}</p>
+            <p className={style.author}>{item?.author}</p>
+            <Link href={item.amazon_product_url} target="_blank">
+              Buy Now →
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
